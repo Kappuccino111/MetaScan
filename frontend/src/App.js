d3a980051ca0e6793130bd990214c6bd707a66fe
Akarshan Kapoor <120595108+Kappuccino111@users.noreply.github.com>
2023-07-02 20:03:01 +0530

    Updating frontend formatting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState} from 'react';
-import axios from 'axios';
 import { io } from 'socket.io-client';
 import './App.css';
+import { fetchInitialData, search, reset, deleteFolder } from './API';
+import SearchForm from './SearchForm';
+import ImagePreview from './ImagePreview';
+import ImageTable from './ImageTable';
 
 const socket = io('http://localhost:5000');
 
@@ -9,112 +12,34 @@ function App() {
   const [data, setData] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
   const [previewImage, setPreviewImage] = useState(null); 
-  
 
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios(
-        'http://localhost:5000/',
-      );
-      setData(result.data);
-    };
-
-    fetchData();
-    socket.on('folder_updated', fetchData);
-
-    // Clean up the effect
-    return () => socket.off('folder_updated', fetchData);
-
+    fetchInitialData(socket, setData);
   }, []);
-
-
-  const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const handleSearchSubmit = async (event) => {
-    event.preventDefault();
-    
-    const result = await axios.post('http://localhost:5000/', {
-      search: searchTerm,
-    });
-
-    setData(result.data);
-  };
-  
-  const handleReset = async () => {
-    const result = await axios.get('http://localhost:5000/');
-    setData(result.data);
-    setSearchTerm('');
-  };
-
-  const handleDelete = async (folderId) => {
-    await axios.post(`http://localhost:5000/delete/${folderId}`);
-    const result = await axios.get('http://localhost:5000/');
-    setData(result.data);
-  };
-
-  const handlePreview = (imageUrl) => {
-    setPreviewImage(imageUrl);  
-  };
-
 
   return (
     <div className="App">
       <h1>Image Metadata Database</h1>
 
-      <form className="search-container" onSubmit={handleSearchSubmit}>
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={handleSearchChange}
-          placeholder="Enter Metadata to Search"
-        />
-        <div className="button-container">
-          <button type="submit">Search</button>
-          <button type="button" onClick={handleReset}>Reset</button>
-        </div>
-      </form>
+      <SearchForm 
+        searchTerm={searchTerm} 
+        setSearchTerm={setSearchTerm}
+        search={search}
+        reset={reset}
+        setData={setData}
+      />
 
-      {previewImage && (
-  <div className="modal">
-    <div className="modal-content">
-      <img src={previewImage} alt="Preview" />
-      <button onClick={() => setPreviewImage(null)}>Close Preview</button>
-    </div>
-  </div>
-)}
+      <ImagePreview 
+        previewImage={previewImage}
+        setPreviewImage={setPreviewImage}
+      />
 
-      <table className="table">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Folder Name</th>
-            <th>Image Preview</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item) => (
-          <tr key={item.id}>
-          <td>{item.id}</td>
-          <td>{item.folder_name}</td>
-          <td>
-            <img className='previewImage'
-              src={`http://localhost:5000/image/${item.folder_name}/image.jpg`}
-              alt="Preview Not Available"
-            />
-          </td>
-          <td>
-             <div className='action'>
-               <button onClick={() => handlePreview(`http://localhost:5000/image/${item.folder_name}/image.jpg`)}>Preview</button>
-                <button onClick={() => handleDelete(item.id)}>Delete</button>
-             </div>
-          </td>
-        </tr>
-))}
-        </tbody>
-      </table>
+      <ImageTable 
+        data={data}
+        setPreviewImage={setPreviewImage}
+        deleteFolder={deleteFolder}
+        setData={setData}
+      />
     </div>
   );
 }
