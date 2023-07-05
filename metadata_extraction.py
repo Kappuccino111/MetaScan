634de83192ecb26e549eecbf61a71e423fd03316
Akarshan Kapoor <120595108+Kappuccino111@users.noreply.github.com>
2023-07-05 17:50:34 +0530

    Update metadata_extraction.py with nltk download caching.

--- metadata_extraction.py
+++ metadata_extraction.py
@@ -13,44 +13,43 @@ import spacy
 
 logging.getLogger('nltk').setLevel(logging.CRITICAL)
 
-# Download necessary NLTK data
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('maxent_ne_chunker')
-nltk.download('words')
-nltk.download('wordnet')
-nltk.download('stopwords')
+# Define NLTK data
+nltk_data = ['punkt', 'averaged_perceptron_tagger', 'maxent_ne_chunker', 'words', 'wordnet', 'stopwords']
 
-# Instantiate the WordNetLemmatizer
+# Check and download necessary NLTK data
+for data in nltk_data:
+    try:
+        nltk.data.find(data)
+    except LookupError:
+        nltk.download(data)
+
+#Instantiate the WordNetLemmatizer for text processing
 lemmatizer = WordNetLemmatizer()
 
 # OCR
 class OCRProcessor:
-    """OCR Processing class."""
+    """Class for Optical Character Recognition."""
+    
     def __init__(self):
-        """Constructor method."""
         self.image_path = None
 
     def preprocess_image(self, image_path):
         """Preprocess image to improve OCR read quality."""
 
-        # Read the image
         image = cv2.imread(image_path)
 
         # Convert the image to grayscale
         grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-        # Apply adaptive thresholding for handling different light conditions
+        # Apply adaptive thresholding
         thresholded_image = cv2.adaptiveThreshold(grayscale_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 
                                                   cv2.THRESH_BINARY, 11, 2)
-
         return thresholded_image
 
     def ocr_core(self, filename):
-        """Handle the core OCR processing of images."""
+        """Core functionality for reading text from images."""
 
         try:
-            # Preprocess the image
             preprocessed_image = self.preprocess_image(filename)
 
             # Perform OCR on the preprocessed image
@@ -64,7 +63,8 @@ class OCRProcessor:
         return text
 
 class MetadataExtractor(OCRProcessor):
-    """Metadata Extractor Class."""
+    """Class for metadata extraction from the the text recieved through OCR."""
+    
     def filter_text(self, text):
         """Filter out non-text characters and punctuation."""
 
@@ -97,35 +97,36 @@ class MetadataExtractor(OCRProcessor):
             entity_dict.setdefault(ent.label_, []).append(ent.text)
         return entity_dict
 
-    def extract_file_metadata(self, filename):
-        """Extract file metadata."""
+    def generate_file_info(self, filename):
+        """Generate file information."""
 
         file_stats = os.stat(filename)
-        metadata = {
+        info = {
             "filename": filename,
             "created": datetime.fromtimestamp(file_stats.st_ctime).strftime("%d/%m/%y, %H:%M:%S"),
             "size": file_stats.st_size
         }
-        return metadata
+        return info
 
     def extract_metadata(self, filename):
-        """Extract all metadata from a file."""
+        """Collect all metadata information and store."""
 
         ocr_text = self.ocr_core(filename)
         filtered_text = self.filter_text(ocr_text)
         preprocessed_text = self.preprocess_text(filtered_text)
 
         entities = self.extract_entities(preprocessed_text)
-        extra_entities_text = str(self.extract_keywords(preprocessed_text))  # Extract extra entities
-        file_metadata = self.extract_file_metadata(filename)
+        extra_entities_text = str(self.extract_keywords(preprocessed_text))  # Extract extra information in case of less named entities
+        file_metadata = self.generate_file_info(filename)
         
         for key in entities:
             file_metadata[key] = entities[key]
             
-        file_metadata['extra_entities'] = extra_entities_text  # Add extra entities to file metadata
+        file_metadata['extra_entities'] = extra_entities_text  
 
         return file_metadata
 
+# File level testing
 if __name__ == "__main__":
     try:
         extractor = MetadataExtractor()
