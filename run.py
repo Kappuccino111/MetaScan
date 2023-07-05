8c77afb1a06dd87d6ff1de694582714c64ffcfba
Akarshan Kapoor <120595108+Kappuccino111@users.noreply.github.com>
2023-07-05 18:45:01 +0530

    Updating Main function.

--- run.py
+++ run.py
@@ -30,10 +30,10 @@ def run_flask_app():
 def run_react_app():
     os.chdir(REACT_APP_DIR)
     global react_process
-    env = os.environ.copy()  # Get a copy of the current environment variables
+    env = os.environ.copy()
     env["BROWSER"] = "none"  # Set BROWSER environment variable
-    react_process = subprocess.Popen(["npm", "start"], env=env)  # Start the react app with the new environment
-    os.chdir("..")  # Change back to the original directory
+    react_process = subprocess.Popen(["npm", "start"], env=env)  
+    os.chdir("..") 
 
 def main_menu():
     while True:
@@ -47,36 +47,33 @@ def main_menu():
 
         if user_input == '1':
             print("Running the scanner... This may take a few moments.")
-            subprocess.call(["python3", "scan.py"])  # Assuming scan.py is in the same directory
+            subprocess.call(["python3", "scan.py"])  
 
         elif user_input == '2':
             print(f"You can now explore the image database by navigating to this URL: http://localhost:3000")
 
         elif user_input == '3':
             print("Shutting down the servers and exiting the program. Bye!")
-            flask_process.terminate()  # Try to gracefully stop the Flask server
-            react_process.terminate()  # Try to gracefully stop the React app
-            time.sleep(6)  # Give the processes some time to stop
+            flask_process.terminate()  # Try to stop the Flask server
+            react_process.terminate()  # Try to stop the React app
+            time.sleep(6)  
             if flask_process.poll() is None:  # If the Flask server is still running
-                flask_process.kill()  # Forcefully stop the Flask server
+                flask_process.kill()  
             if react_process.poll() is None:  # If the React app is still running
-                react_process.kill()  # Forcefully stop the React app
-            os._exit(0)  # Use os._exit to terminate all threads
+                react_process.kill()  
+            os._exit(0)  # Terminate all threads
 
         else:
             print("Error: Invalid input! Please enter a number between 1 and 3.")
 
-def main():
+if __name__ == '__main__':
     try:
         create_directory()
         run_react_app()  # Run the React app
-        t = threading.Thread(target=run_flask_app)
+        t = threading.Thread(target=run_flask_app) # Run the Flask server
         t.start()
         print("Starting the servers... This may take a few moments.")
         time.sleep(6)  # Give the server a bit of time to start
         main_menu()
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
-
-if __name__ == '__main__':
-    main()
