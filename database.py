3d612c6ee48f350bdad67f8527d63e2094f0909e
Akarshan Kapoor <120595108+Kappuccino111@users.noreply.github.com>
2023-07-05 18:10:33 +0530

    Updating database.py

--- database.py
+++ database.py
@@ -1,15 +1,17 @@
 from config import local_storage, sqlite3
 
 def get_db():
-    """Get a SQLite database connection."""
+    """Connect to the database through SQLite."""
+    
     if not hasattr(local_storage, 'db'):
         local_storage.db = sqlite3.connect('image_database.db')
-        local_storage.db.execute('PRAGMA foreign_keys = ON')  # Enable foreign key support
+        local_storage.db.execute('PRAGMA foreign_keys = ON')  # Enable referential integrity
     return local_storage.db
 
 
 def create_table():
-    """Create a table if it doesn't exist."""
+    """Create a datbase table if it doesn't exist."""
+    
     database = get_db()
     database.execute('''
         CREATE TABLE IF NOT EXISTS images (
