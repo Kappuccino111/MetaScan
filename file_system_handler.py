784f260fe6af48d31f04d944855421cb35605a5e
Akarshan Kapoor <120595108+Kappuccino111@users.noreply.github.com>
2023-07-05 18:19:50 +0530

    Update file_system_handler.py with comment updates

--- file_system_handler.py
+++ file_system_handler.py
@@ -1,28 +1,27 @@
 import os
 import time
 from watchdog.events import FileSystemEventHandler
-
 from config import socketio
 from database import get_db
 
 class FolderEventHandler(FileSystemEventHandler):
-    """Event handler for new folder creation and deletion."""
+    """Event handling for folder creation and deletion in the target folder."""
 
     def on_created(self, event):
         """Called when a directory is created."""
+        
         if event.is_directory:
             folder_path = event.src_path
             folder_name = os.path.basename(folder_path)
 
-            # Wait until metadata.txt file is available
+            # Wait for metadata.txt to come into existence 
             while not os.path.exists(os.path.join(folder_path, 'metadata.txt')):
                 time.sleep(0.1)
 
-            # Read the metadata from the text file
             with open(os.path.join(folder_path, 'metadata.txt'), 'r', encoding='utf-8') as file:
                 metadata = file.read()
 
-            # Insert the folder name and metadata into the database
+            # Insert the folder name and metadata into the SQLite database
             database = get_db()
             database.execute('INSERT INTO images (folder_name, metadata) VALUES (?, ?)',
              (folder_name, metadata))
@@ -31,11 +30,12 @@ class FolderEventHandler(FileSystemEventHandler):
 
     def on_deleted(self, event):
         """Called when a directory is deleted."""
+        
         if event.is_directory:
             folder_path = event.src_path
             folder_name = os.path.basename(folder_path)
 
-            # Delete the corresponding record from the database
+            # Delete the corresponding record from the SQLite database
             database = get_db()
             database.execute('DELETE FROM images WHERE folder_name = ?', (folder_name,))
             database.commit()
