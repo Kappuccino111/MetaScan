aabc911385b3f1c517782bc46fbbd4692cef61f4
Akarshan Kapoor <120595108+Kappuccino111@users.noreply.github.com>
2023-07-05 17:59:02 +0530

    Update config.py with logging configuration changes

--- config.py
+++ config.py
@@ -12,17 +12,21 @@ from flask_cors import CORS
 from flask_socketio import SocketIO
 from werkzeug.utils import secure_filename
 
-logging.getLogger('socketio').setLevel(logging.CRITICAL)
-logging.getLogger('engineio').setLevel(logging.CRITICAL)
-log = logging.getLogger('werkzeug')
-log.setLevel(logging.ERROR)
-
 app = Flask(__name__)
 
+# Apply Cross-Origin Resource Sharing to limit accepting requests from the localhost at port 3000.
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 # Create thread-local storage for the database connection
 local_storage = threading.local()
-
 SCAN_DIRECTORY = "scannedImages"
+
+# Update login configurations
+logging.basicConfig(level=logging.ERROR,
+                    format='%(asctime)s:%(levelname)s:%(name)s: %(message)s')
+                    
+loggers = ['socketio', 'engineio', 'werkzeug']
+
+for logger in loggers:
+    logging.getLogger(logger).setLevel(logging.CRITICAL)
